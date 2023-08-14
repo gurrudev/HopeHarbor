51cc6375c45d4a233c013c3d2b547816c83d4be1
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-08-14 19:37:55 +0530

    updated

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import {Navbar, Sidebar} from './components'
+
+import { CampaignDetails, CreateCampaign, Profile, Home,  } from './pages'
+import './index.css'
+
+function App() {
+  return (
+    <div className='relative sm: -8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+        
+        <div className="sm:flex hidden mr10 relative">
+            <Sidebar/>
+        </div>
+
+        <div className="flex-1 max-sm: w-full max-w[1280px] mx-auto sm:pr-5">
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+            </Routes>
+        </div>
+        
+    </div>
+  )
+}
+
+export default App
