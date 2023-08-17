38b2fb26342172a467fec38ed88dd3c2849fefd7
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-08-17 18:43:32 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,19 +8,21 @@ import './index.css'
 
 function App() {
   return (
-    <div className='relative sm: -8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-        
-        <div className="sm:flex hidden mr10 relative">
-            <Sidebar/>
-        </div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
 
-        <div className="flex-1 max-sm: w-full max-w[1280px] mx-auto sm:pr-5">
-            <Navbar/>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-            </Routes>
-        </div>
-        
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
     </div>
   )
 }
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,23 +1,32 @@
 export const daysLeft = (deadline) => {
-    const difference = new Date(deadline).getTime() - Date.now();
-    const remainingDays = difference / (1000 * 3600 * 24);
-  
-    return remainingDays.toFixed(0);
-  };
-  
-  export const calculateBarPercentage = (goal, raisedAmount) => {
-    const percentage = Math.round((raisedAmount * 100) / goal);
-  
-    return percentage;
-  };
-  
-  export const checkIfImage = (url, callback) => {
-    const img = new Image();
-    img.src = url;
-  
-    if (img.complete) callback(true);
-  
-    img.onload = () => callback(true);
-    img.onerror = () => callback(false);
-  };
-  
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainingDays = difference / (1000 * 3600 * 24);
+
+  return remainingDays.toFixed(0);
+};
+
+export const calculateBarPercentage = (goal, raisedAmount) => {
+  const percentage = Math.round((raisedAmount * 100) / goal);
+
+  return percentage;
+};
+
+export const checkIfImage = (url, callback) => {
+  const img = new Image();
+  img.src = url;
+
+  if (img.complete) callback(true);
+
+  img.onload = () => callback(true);
+  img.onerror = () => callback(false);
+};
+
+export const formattedDate = (dateStr) => {
+  let dateObj = new Date(dateStr);
+  let day = dateObj.getDate().toString().padStart(2, "0");
+  let month = (dateObj.getMonth() + 1).toString().padStart(2, "0");
+  let year = dateObj.getFullYear();
+  let formattedDate = day + "-" + month + "-" + year;
+
+  return formattedDate;
+};
