88d1918a2cf22a024ec931ff6eea761692a61c10
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-08-20 19:38:08 +0530

    creation of campaigns and connection with metamask✨

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useStateContext } from '../context'
 
 function Home() {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [campaign, setCampaign] = useState([])
+
+  const {address, contract, getCampaigns} = useStateContext()
+
+  const fetchCampaigns =  async() =>{
+    setIsLoading(true)
+    const data = await getCampaigns()
+    setCampaign(data)
+    setIsLoading(false)
+  }
+
+  useEffect(() =>{
+    if(contract) fetchCampaigns()
+  },[address, contract])
+
   return (
     <div>Homie</div>
   )
