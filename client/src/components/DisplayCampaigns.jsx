88541862794027e3d4c50dd49a9578baa81645ab
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-08-21 18:40:56 +0530

    updated

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { loader } from '../assets'
 import { useNavigate } from 'react-router-dom'
+import {FundCard} from '../components'
 
 const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
 
