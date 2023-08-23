9508defa9e8798911c6267bff9780ab563ee40d9
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2023-08-23 16:10:40 +0530

    updated

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import { profileImg } from '../assets'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useStateContext } from '../context'
 import { daysLeft, calculateBarPercentage } from '../utils'
-import { CountBox, CustomButton } from '../components'
+import { CountBox, CustomButton, Loader } from '../components'
 
 function CampaignDetails() {
   const { state } = useLocation()
   const {donate, getDonations, contract, address } = useStateContext()
-
+  const navigate = useNavigate()
   const [isLoading, setIsLoading] = useState(false)
   const [amount, setAmount] = useState('')
   const [donators, setDonators] = useState([])
@@ -25,6 +25,7 @@ function CampaignDetails() {
   const handleDonate = async() =>{
     setIsLoading(true) 
     await donate(state.pId, amount)
+    navigate('/')
     setIsLoading(false)
   }
 
@@ -34,14 +35,13 @@ function CampaignDetails() {
 
   return (
     <div>
-      {isLoading && 'Loading...'}
+      {isLoading && <Loader/>}
 
       <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
-        <div className='flex-1 flex-col'>
-          <img src={state.image} alt="campaign" className='w-full h-[410px] object-cover rounded-xl' />
-          <div className='relative w-full h-[5px] bg-[#3a3a43] mt-2 rounded'>
-            <div className='absolute h-full bg-[#4acd8d] rounded' style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)} %`, maxWidth: `100%` }}>
-
+      <div className="flex-1 flex-col">
+          <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl"/>
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2 rounded">
+            <div className="absolute h-full bg-[#4acd8d] rounded" style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
             </div>
           </div>
         </div>
@@ -77,18 +77,16 @@ function CampaignDetails() {
 
           <div>
             <h4 className='font-epilogue text-left font-semibold text-[18px] uppercase'>Donators</h4>
-            <div className='mt-20px flex flex-col gap-4'>
-
-              {donators.length > 0 ? donators.map((item, 
-                index) =>{
-                    <div>
-                      DONATOR
-                    </div>
-                }) : (
-                  <p className='font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify'>No donators yet. Be the first one!</p>
-                )
-              }
-            </div>
+            <div className="mt-[20px] flex flex-col gap-4">
+                {donators.length > 0 ? donators.map((item, index) => (
+                  <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4">
+                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator}</p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation}</p>
+                  </div>
+                )) : (
+                  <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
+                )}
+              </div>
           </div>
         </div>
 
